fd9d840ab6c0d58a50368d4dd330eef2a99f8630
psmever <psmever@gmail.com>
2020-11-04 00:08:24 +0900

    [ADD] Base Network Manager.

--- NicePageBlog/Network/API.swift
+++ NicePageBlog/Network/API.swift
@@ -30,7 +30,8 @@ final class Api {
             case .success(let result):
                 let access_token: String = result.access_token
                 let refresh_token: String = result.refresh_token
-
+                let expires_in: Int = result.expires_in
+                
                 if(access_token.count > 0 && refresh_token.count > 0) {
                     self.userDefaultsManager.setAccessToken(token: access_token)
                     self.userDefaultsManager.setRefreshToken(token: refresh_token)
@@ -45,20 +46,16 @@ final class Api {
         }
     }
     
-    func loginCheck() {
+    func loginCheck(completion: @escaping (Bool) -> Void) {
         NetworkManager.shared.request(Environment.ServerBaseURL.absoluteString + "/api/v1/auth/login-check", method: .get) { ( result: Result<LoginCheck, ErrorResponse> ) in
-            print(":: loginCheck RESULT START ::")
             switch result{
             case .success(let result):
-                
-                debugPrint(result)
-
-
+                debugPrint("login check user_uuid: \(result.user_uuid)")
+                completion(true)
             case .failure(let error):
                 debugPrint("\(error.error_message)")
-
+                completion(false)
             }
-            print(":: loginCheck RESULT END ::")
         }
     }
     
