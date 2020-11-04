24ebab685139bef7af43462e440baf7239b82eb6
psmever <psmever@gmail.com>
2020-11-05 07:19:58 +0900

    [Working] Login Scene.

--- NicePageBlog/NicePageBlogApp.swift
+++ NicePageBlog/NicePageBlogApp.swift
@@ -18,7 +18,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//        UserDefaultsManager.shared.deleteAllData()
+        UserDefaultsManager.shared.deleteAllData()
 
         #if DEBUG
             debugPrint(":: Start Debug ::")
